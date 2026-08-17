6fef214b62a72ea14a9d0493d550883bc39ad653
timmmtam <tys.timm@gmail.com>
2026-08-17 11:01:03 +0800

    fix: change print flag for philo position to %zu (for size_t)

--- srcs/utils/print_log.c
+++ srcs/utils/print_log.c
@@ -6,7 +6,7 @@
 /*   By: timtan <timtan@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 18:25:50 by timtan            #+#    #+#             */
-/*   Updated: 2026/08/16 09:48:11 by timtan           ###   ########.fr       */
+/*   Updated: 2026/08/17 10:59:49 by timtan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	print_log(t_philo *philo, char *message)
 {
 	pthread_mutex_lock(&philo->print_lock);
-	printf("%lldms %d %s\n", current_time_in_ms(), philo->position, message);
+	printf("%lldms %zu %s\n", current_time_in_ms(), philo->position, message);
 	pthread_mutex_unlock(&philo->print_lock);
 }
