da2a41ffb53da4ffe2c809b5493c015ae9c674df
timmmtam <tys.timm@gmail.com>
2026-08-30 14:12:28 +0800

    refactor(ft_atol): remove redundant number check

--- srcs/utils/ft_atol.c
+++ srcs/utils/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: timtan <timtan@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 16:37:50 by timtan            #+#    #+#             */
-/*   Updated: 2026/08/30 14:07:24 by timtan           ###   ########.fr       */
+/*   Updated: 2026/08/30 14:11:58 by timtan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,6 @@ long	ft_atol(const char *nptr)
 			sign *= -1;
 		i++;
 	}
-	if (nptr[i] < '0' || nptr[i] > '9')
-		return (0);
 	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		if (result > (LONG_MAX - (nptr[i] - '0')) / 10)
